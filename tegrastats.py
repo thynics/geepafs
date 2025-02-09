380295297a224abbe0b5d21666f92daa823021d8
Cheng Long <thynics@gmail.com>
2025-02-09 19:17:04 +0100

    debug

--- tegrastats.py
+++ tegrastats.py
@@ -2,13 +2,17 @@ import subprocess
 
 def monitor_tegrastats(output_file):
     command = ["sudo tegrastats"]
-    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True) as proc:
-        try:
-            for line in proc.stdout:
-                print(line)
-        except KeyboardInterrupt:
-            print("terminate tegrastats")
-            proc.terminate()
+    with open(output_file, "w") as file:
+        with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True) as proc:
+            try:
+                for line in proc.stdout:
+                    file.seek(0)
+                    file.truncate()
+                    file.write(line.strip() + "\n")
+                    file.flush()
+                    
+            except KeyboardInterrupt:
+                proc.terminate()
 
 if __name__ == "__main__":
     output_file_path = "tegrastats_output.txt"
