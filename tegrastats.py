1643c900f7f672a83e3e60ab304ddef63c6d0675
Cheng Long <thynics@gmail.com>
2025-02-09 19:21:57 +0100

    debug

--- tegrastats.py
+++ tegrastats.py
@@ -6,9 +6,10 @@ def monitor_tegrastats(output_file):
         with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True) as proc:
             try:
                 for line in proc.stdout:
+                    decoded_line = line.decode('utf-8').strip()
                     file.seek(0)
                     file.truncate()
-                    file.write(str(line.strip()))
+                    file.write(decoded_line.strip())
                     file.flush()
                     
             except KeyboardInterrupt:
