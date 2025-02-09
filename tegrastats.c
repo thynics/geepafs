9e76264f7dc8491a3e547a0945689f060e21d6fb
Cheng Long <thynics@gmail.com>
2025-02-09 18:53:02 +0100

    debug

--- tegrastats.c
+++ tegrastats.c
@@ -213,6 +213,7 @@ int tegrastats_init() {
             perror("pthread_create failed");
             return 1;
         }
+        sleep(5000);
         // *shared_memory = 1;  // Initialize shared memory and set value to 1
     } else {
         printf("Shared memory already initialized. Skipping init.\n");
